95ca6d58a635c51636e4c2114e975d9f484229fd
bracken <bracken@instructure.com>
2010-02-19 10:40:38 -0700

    Changed the rake tast to take a queue argument. Updated the readme to show how to run workers for queues.

--- lib/delayed/tasks.rb
+++ lib/delayed/tasks.rb
@@ -5,11 +5,13 @@ task :merb_env
 namespace :jobs do
   desc "Clear the delayed_job queue."
   task :clear => [:merb_env, :environment] do
+    #todo: make it so that queues can be specified
     Delayed::Job.delete_all
   end
 
-  desc "Start a delayed_job worker."
-  task :work => [:merb_env, :environment] do
-    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY'], :queue => ENV['QUEUE']).start
+  desc "Start a delayed_job worker. You can specify which queue to process from, for example: rake jobs:work[my_queue], or: QUEUE=my_queue rake jobs:work"
+  task :work, [:queue] => [:merb_env, :environment] do |t,args|
+    queue = args.queue || ENV['QUEUE']
+    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY'], :queue => queue).start
   end
 end
