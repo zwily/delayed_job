141c9ec42d043dd42000ddb1dffddf4e61fc320f
Brian Palmer <brianp@instructure.com>
2010-09-22 11:04:20 -0600

    fix spec broken by daylight savings

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -21,7 +21,7 @@ describe Delayed::Job do
   context "db_time_now" do
     it "should return time in current time zone if set" do
       Time.zone = 'Eastern Time (US & Canada)'
-      Delayed::Job.db_time_now.zone.should == 'EST'
+      Delayed::Job.db_time_now.zone.should match(/EST|EDT/)
     end
     
     it "should return UTC time if that is the AR default" do
@@ -33,7 +33,7 @@ describe Delayed::Job do
     it "should return local time if that is the AR default" do
       Time.zone = 'Central Time (US & Canada)'
       ActiveRecord::Base.default_timezone = :local
-      Delayed::Job.db_time_now.zone.should == 'CST'
+      Delayed::Job.db_time_now.zone.should match(/CST|CDT/)
     end
   end
   
