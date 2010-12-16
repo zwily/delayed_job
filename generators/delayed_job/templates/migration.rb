3aec95615080598f3c293015a28577efc36cf540
Brian Palmer <brianp@instructure.com>
2010-12-16 16:09:51 -0700

    reformatting and migration fixup
    By setting the migration connection explicitly, this works even when the delayed
    jobs queue is on a different db connection.

--- generators/delayed_job/templates/migration.rb
+++ generators/delayed_job/templates/migration.rb
@@ -1,22 +1,38 @@
 class CreateDelayedJobs < ActiveRecord::Migration
+  def self.connection
+    Delayed::Backend::ActiveRecord::Job.connection
+  end
+
   def self.up
-    create_table :delayed_jobs, :force => true do |table|
-      table.integer  :priority, :default => 0      # Allows some jobs to jump to the front of the queue
-      table.integer  :attempts, :default => 0      # Provides for retries, but still fail eventually.
-      table.text     :handler                      # YAML-encoded string of the object that will do work
-      table.text     :last_error                   # reason for last failure (See Note below)
-      table.string   :queue, :default => nil       # The queue that this job is in
-      table.datetime :run_at                       # When to run. Could be Time.zone.now for immediately, or sometime in the future.
-      table.datetime :locked_at                    # Set when a client is working on this object
-      table.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
-      table.string   :locked_by                    # Who is working on this object (if locked)
+    create_table :delayed_jobs do |table|
+      # Allows some jobs to jump to the front of the queue
+      table.integer  :priority, :default => 0
+      # Provides for retries, but still fail eventually.
+      table.integer  :attempts, :default => 0
+      # YAML-encoded string of the object that will do work
+      table.text     :handler, :limit => 500.kilobytes
+      # reason for last failure (See Note below)
+      table.text     :last_error
+      # The queue that this job is in
+      table.string   :queue, :default => nil
+      # When to run.
+      # Could be Time.zone.now for immediately, or sometime in the future.
+      table.datetime :run_at
+      # Set when a client is working on this object
+      table.datetime :locked_at
+      # Set when all retries have failed
+      table.datetime :failed_at
+      # Who is working on this object (if locked)
+      table.string   :locked_by
+
       table.timestamps
     end
-	
+
     add_index :delayed_jobs, [:priority, :run_at], :name => 'delayed_jobs_priority'
+    add_index :delayed_jobs, [:queue], :name => 'delayed_jobs_queue'
   end
-  
+
   def self.down
-    drop_table :delayed_jobs  
+    drop_table :delayed_jobs
   end
 end
